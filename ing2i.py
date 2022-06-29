1baae182359f069b34a8a3498e9b5d69441bb83d
Sol Ayala <solmacayala@gmail.com>
2022-06-29 18:55:04 -0300

    cambio

---
+++ ing2i.py
@@ -0,0 +1,4 @@
+print("Comencemos con nuestro viaje...")
+numero1=int(input("Por favor ingrese el primer numero entero: "))
+numero2=int(input("Por favor ingrese el segundo numero entero: "))
+print("Los numeros ingresados son: ",numero1, numero2)
