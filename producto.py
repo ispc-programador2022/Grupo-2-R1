40781bcc142045b1cd5039827c667b74f0b37a66
Sol Ayala <solmacayala@gmail.com>
2022-06-29 19:30:41 -0300

    listo

---
+++ producto.py
@@ -0,0 +1,6 @@
+print("Vamos a realizar un producto entre dos valores")
+numero1=float(input("Por favor ingrese el primer numero : "))
+numero2=float(input("Por favor ingrese el segundo numero: "))
+producto=numero1*numero2
+print("Los numeros ingresados son: ",numero1, numero2)
+print("el producto entre los dos valores es: ",producto)
