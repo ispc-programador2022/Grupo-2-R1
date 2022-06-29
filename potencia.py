18bace243b0fb3fed9c3ae695227b5f32200f1ab
Sol Ayala <solmacayala@gmail.com>
2022-06-29 20:57:07 -0300

    commit

---
+++ potencia.py
@@ -0,0 +1,6 @@
+print("Vamos a realizar una potencia del primer valor elevado al segundo valor")
+numero1=float(input("Por favor ingrese el primer valor : "))
+numero2=float(input("Por favor ingrese el segundo valor: "))
+potencia=numero1**numero2
+print("Los numeros ingresados son: ",numero1, numero2)
+print("la potencia entre los dos valores es: ",potencia)
