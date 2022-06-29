32ccd0b60930858e29d672c30cae0f9db1a4caea
Sol Ayala <solmacayala@gmail.com>
2022-06-29 19:07:23 -0300

    hecho

---
+++ ing2s.py
@@ -0,0 +1,3 @@
+str1=str(input("Por favor ingrese el primer valor string: "))
+str2=str(input("Por favor ingrese el segundo valor string: "))
+print("Los valores string ingresados son: ",str1, str2)
