11d29b2e6bac83d18d6d2c0ea0003373c34f3729
Sol Ayala <solmacayala@gmail.com>
2022-06-29 20:50:07 -0300

    commit

--- cociente.py
+++ cociente.py
@@ -1,5 +1,5 @@
 print("Realizamos un cociente entre dos valores")
-num1=int(input("Igrese el primer numero: "))
-num2=int(input("Ingrese el segundo numero: "))
+num1=float(input("Igrese el primer numero: "))
+num2=float(input("Ingrese el segundo numero: "))
 cociente=num1//num2
 print("El cociente entre el primer numero y el segundo numero es de: ",cociente)
---
+++ modulo.py
@@ -0,0 +1,6 @@
+print("Realizamos el modulo entre dos numeros")
+num1=int(input("Igrese el primer numero: "))
+num2=int(input("Ingrese el segundo numero: "))
+modulo=num1%num2
+print("Los valores ingresados son: ",num1,num2)
+print("El cociente entre el primer numero y el segundo numero es de: ",modulo)
