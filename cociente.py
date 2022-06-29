7d928c1f5bb21708023e73f193dbfa93c338b826
Sol Ayala <solmacayala@gmail.com>
2022-06-29 20:41:25 -0300

    commit

---
+++ cociente.py
@@ -0,0 +1,5 @@
+print("Realizamos un cociente entre dos valores")
+num1=int(input("Igrese el primer numero: "))
+num2=int(input("Ingrese el segundo numero: "))
+cociente=num1//num2
+print("El cociente entre el primer numero y el segundo numero es de: ",cociente)
