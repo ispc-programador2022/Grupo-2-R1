ec41f1dd9c18dc907c5ff523e81bf3342005973f
Sol Ayala <solmacayala@gmail.com>
2022-06-29 18:33:00 -0300

    primer

---
+++ cartel.py
@@ -0,0 +1,1 @@
+print("-----------------------------------------------------------------------\nBienvenidos al grupo A2G1!\nEsperamos que su pasantia en nuestro espacio sea de su agrado :D\n-----------------------------------------------------------------------")
