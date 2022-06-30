36a75835337934d29c844bf5ad30b1ed67c5d33c
Sol Ayala <solmacayala@gmail.com>
2022-06-29 21:25:43 -0300

    commit

---
+++ radicacion.py
@@ -0,0 +1,7 @@
+print("Retorna la raiz del primer respecto al segundo valor")
+
+num1=int(input("ingrese el primer valor: "))
+num2=int(input("Ingrese la raiz deseada: "))
+b=1/num2
+
+print(pow(num1,b)) #la funcion pw me toma el primer parametro como base y el segundo la raiz
