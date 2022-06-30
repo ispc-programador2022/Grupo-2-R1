75c093f1369726664b690e466053495ce608a0a2
Sol Ayala <solmacayala@gmail.com>
2022-06-29 22:48:10 -0300

    commit

--- p1resta.py
+++ p1resta.py
@@ -1,8 +1,8 @@
-print("Retorna la suma de los dos primeros parametros por el tercer parametro")
+print("Retorna el producto de los dos primeros parametros mas el tercer parametro")
 
 a=float(input("Ingresar el primer numero: "))
 b=float(input("Ingresar el segundo numero: "))
 c=float(input("Ingresar el tercer numero: "))
-total=(a+b)*c
+total=(a-b)*c
 print("Los valores ingresados son: ",a,b,c)
 print("El total es de: ",total)
