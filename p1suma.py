5102c2fd6f040e84c6562d6efbb8541a7feb713c
Sol Ayala <solmacayala@gmail.com>
2022-06-29 22:03:41 -0300

    commit

---
+++ p1suma.py
@@ -0,0 +1,7 @@
+print("Retorna el producto de los dos primeros parametros mas el tercer parametro")
+a=float(input("Ingresar el primer numero: "))
+b=float(input("Ingresar el segundo numero: "))
+c=float(input("Ingresar el tercer numero: "))
+total=(a*b)+c
+print("Los valores ingresados son: ",a,b,c)
+print("El total es de: ",total)
