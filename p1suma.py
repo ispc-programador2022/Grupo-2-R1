b5e59f0a8535cb567fda7a79c569188293e2ce92
Sol Ayala <solmacayala@gmail.com>
2022-07-27 20:11:09 -0300

    commit

--- p1suma.py
+++ p1suma.py
@@ -1,19 +1,15 @@
-
 from suma import suma
 
+def funcion_p1_suma ():
+    
+    num3=int(input("Ingrese el tercer valor: "))
+    suma_total=suma()*num3
+    print("El resultado total es: ",suma_total)
+    return suma_total
 
-def funcion_p1_suma (a,b,num3):
-  
- funcion_p1_suma=suma(a,b)*num3
- print("El resultado total es: ",funcion_p1_suma)
- return funcion_p1_suma
+funcion_p1_suma()
 
-a=int((input("Ingresar el primer valor: ")))
 
-b=int((input("Ingresar el segundo valor: ")))
-suma(a,b)
-num3=int(input("Ingrese el tercer valor: "))
-funcion_p1_suma(a,b,num3)
 
 
 	
--- suma.py
+++ suma.py
@@ -1,12 +1,11 @@
-print("hola")
+print("Se busca realizar la suma entre dos valores, a y b ")
 def suma ():
     a=int((input("Ingresar el primer valor: ")))
 
     b=int((input("Ingresar el segundo valor: ")))
     suma2 = a + b
     print("El resultado de la suma es: ",suma2)
-    return suma
-
+    return suma2
 
 
 
