04dc4d6fea289d358ffe0ae8fb39d07650f6aed4
Sol Ayala <solmacayala@gmail.com>
2022-07-27 19:20:43 -0300

    commit

--- suma.py
+++ suma.py
@@ -1,11 +1,12 @@
 print("hola")
-def suma (a,b):
-    suma = a + b
-    print("El resultado de la suma es: ",suma)
+def suma ():
+    a=int((input("Ingresar el primer valor: ")))
+
+    b=int((input("Ingresar el segundo valor: ")))
+    suma2 = a + b
+    print("El resultado de la suma es: ",suma2)
     return suma
 
-a=int((input("Ingresar el primer valor: ")))
 
-b=int((input("Ingresar el segundo valor: ")))
-suma(a,b)
+
 
