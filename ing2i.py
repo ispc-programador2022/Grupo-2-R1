5c60d7aca57c25d3296fe6f7efa6170f475c5974
Brian <polobrallardbrian@gmail.com>
2022-07-29 00:30:42 -0300

    agregue punto 22 y punto 28 y modifique ing2i e ing2s

--- ing2i.py
+++ ing2i.py
@@ -1,4 +1,6 @@
-print("Comencemos con nuestro viaje...")
-numero1=int(input("Por favor ingrese el primer numero entero: "))
-numero2=int(input("Por favor ingrese el segundo numero entero: "))
-print("Los numeros ingresados son: ",numero1, numero2)
+
+def ing2i():
+    print("Comencemos con nuestro viaje...")
+    numero1=int(input("Por favor ingrese el primer numero entero: "))
+    numero2=int(input("Por favor ingrese el segundo numero entero: "))
+    print("Los numeros ingresados son: ",numero1, numero2)
--- ing2s.py
+++ ing2s.py
@@ -1,3 +1,4 @@
-str1=str(input("Por favor ingrese el primer valor string: "))
-str2=str(input("Por favor ingrese el segundo valor string: "))
-print("Los valores string ingresados son: ",str1, str2)
+def ing2s():
+    str1=str(input("Por favor ingrese el primer valor string: "))
+    str2=str(input("Por favor ingrese el segundo valor string: "))
+    print("Los valores string ingresados son: ",str1, str2)
